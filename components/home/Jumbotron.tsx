7a129d65b4c6fa344e3466b6a66c9f393afda9fb
Amit Sen <amit@atlasai.us>
2021-09-10 11:26:41 -0700

    Create jumbotron

---
+++ components/home/Jumbotron.tsx
@@ -0,0 +1,14 @@
+import {Button} from 'antd';
+import React from 'react';
+
+const Jumbotron = () => {
+  return (
+    <div>
+      <h1>Cameroon for the Future</h1>
+      <h2>Explore the data</h2>
+      <Button>Explore the data</Button>
+    </div>
+  );
+};
+
+export default Jumbotron;
