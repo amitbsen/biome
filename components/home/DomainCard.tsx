df156c509d535aaa599b6d19a2f222bb91372a7d
Amit Sen <amit@atlasai.us>
2021-09-10 13:51:26 -0700

    Create DomainDisplay and DomainCard

---
+++ components/home/DomainCard.tsx
@@ -0,0 +1,33 @@
+import {Button, Divider} from 'antd';
+import React from 'react';
+import Separator from '../shared/Separator';
+import Link from 'next/link';
+
+interface DomainCardComponentProps {
+  name: string;
+  description: string;
+  link: string;
+}
+
+const DomainCard = (props: DomainCardComponentProps) => {
+  const {name, description, link} = props;
+  return (
+    <Link href="/datasets">
+      <div
+        className="bg-gray-400 text-white text-center rounded grid grid-rows pt-12 pb-24 justify-items-center cursor-pointer"
+        style={{height: 488}}
+      >
+        <Separator color="white" width={30} />
+        <div className="text-center">
+          <h2 className="text-lg text-white">{name}</h2>
+          <p>{description}</p>
+        </div>
+        <div>
+          <p className="mb-0 py-1 px-4 border-2 text-sm">Explore</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default DomainCard;
